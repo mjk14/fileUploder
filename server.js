01cf5d80fded6c5f37f7b518ce1f86ef234c6bb3
mjk14 <majidkakavandi12776@gmail.com>
2019-12-26 04:11:11 +0330

    add helmet for seucreable express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express=require('express');
+const helmet = require('helmet');
 const app=express();
 const controller=require('./controller');
 
 //middlewares
+app.use(helmet());//for security configuration
 const { uploadImage } = require('./middlewares/uploadImage');
 const auth=require('./middlewares/auth.js');
 
@@ -13,6 +15,7 @@ app.post('/user/upload_image',auth,uploadImage.single('image'),controller.save.b
 app.post('/user/get_image',auth,controller.download.bind(controller));
 app.post('/user/delete_image',auth,controller.delete.bind(controller));
 
+//web server
 const port=process.env.APPLICATION_PORT;
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
